2dcc8be910a5e22772d318e0030b10261f0a2363
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-02-21 20:00:11 +0000

    deploy

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('  https://stormy-forest-91643.herokuapp.com/signin', {
+    fetch('https://stormy-forest-91643.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
